242ba5131f7a9d32acfa1db7509fc5a1c0281bad
kumudun <kumudun@metropolia.fi>
2026-03-06 00:59:55 +0200

    Update init.sql to drop existing calc_results table and add new columns for subtraction and division results

--- init.sql
+++ init.sql
@@ -1,11 +1,14 @@
-CREATE DATABASE IF NOT EXISTS calc_data;
 USE calc_data;
 
-CREATE TABLE IF NOT EXISTS calc_results (
-                                            id INT AUTO_INCREMENT PRIMARY KEY,
-                                            number1 DOUBLE NOT NULL,
-                                            number2 DOUBLE NOT NULL,
-                                            sum_result DOUBLE NOT NULL,
-                                            product_result DOUBLE NOT NULL,
-                                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+DROP TABLE IF EXISTS calc_results;
+
+CREATE TABLE calc_results (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  number1 DOUBLE NOT NULL,
+  number2 DOUBLE NOT NULL,
+  sum_result DOUBLE NOT NULL,
+  product_result DOUBLE NOT NULL,
+  subtract_result DOUBLE NOT NULL,
+  division_result DOUBLE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
